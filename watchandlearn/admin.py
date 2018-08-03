f6159a69808405ed3d9b6d586f00c51fb835eb44
Sanket Padmanabhan <sanketq@berkeley.edu>
2018-08-03 13:29:42 -0700

    Registered models with admin

--- watchandlearn/admin.py
+++ watchandlearn/admin.py
@@ -5,3 +5,10 @@ from watchandlearn.models import *
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Episode)
+admin.site.register(Series)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Topic)
+admin.site.register(Interest)
+
