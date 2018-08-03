d5c27cac05361bf160d8cabb25d9b67bc3c1d674
Sanket Padmanabhan <sanketq@berkeley.edu>
2018-08-03 11:39:33 -0700

    Added basic code skeleton

--- watchandlearn/views.py
+++ watchandlearn/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.core import serializers
+from django.views import generic
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from .forms import *
+from watchandlearn.models import *
 
-# Create your views here.
+
+def index(request):
+    return render(
+        request,
+        'watchandlearn/index.html',
+        context={},
+    ) 
+
+# @login_required
