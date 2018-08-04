16d8f0461e443f8ef0e5f56201da64cf96845a1c
Sanket Padmanabhan <sanketq@gmail.com>
2018-08-03 17:37:28 -0700

    Added import export funcitonality for all models

--- watchandlearn/admin.py
+++ watchandlearn/admin.py
@@ -16,15 +16,63 @@ class WordResource(resources.ModelResource):
 class WordAdmin(ImportExportModelAdmin):
     resource_class = WordResource
 
+class EpisodeResource(resources.ModelResource):
+
+    class Meta:
+        model = Episode
+
+class EpisodeAdmin(ImportExportModelAdmin):
+    resource_class = EpisodeResource
+
+class SeriesResource(resources.ModelResource):
+
+    class Meta:
+        model = Series
+
+class SeriesAdmin(ImportExportModelAdmin):
+    resource_class = SeriesResource
+
+class QuizResource(resources.ModelResource):
+
+    class Meta:
+        model = Quiz
+
+class QuizAdmin(ImportExportModelAdmin):
+    resource_class = QuizResource
+
+class QuestionResource(resources.ModelResource):
+
+    class Meta:
+        model = Question
+
+class QuestionAdmin(ImportExportModelAdmin):
+    resource_class = QuestionResource
+
+class TopicResource(resources.ModelResource):
+
+    class Meta:
+        model = Topic
+
+class TopicAdmin(ImportExportModelAdmin):
+    resource_class = TopicResource
+
+class InterestResource(resources.ModelResource):
+
+    class Meta:
+        model = Interest
+
+class InterestAdmin(ImportExportModelAdmin):
+    resource_class = InterestResource
+
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Word, WordAdmin)
-admin.site.register(Episode)
-admin.site.register(Series)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Topic)
-admin.site.register(Interest)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Interest, InterestAdmin)
 
